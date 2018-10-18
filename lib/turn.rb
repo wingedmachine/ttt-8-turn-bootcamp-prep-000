e387d1435031aa69002fd9c128093e8eadb65638
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-18 00:53:37 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+def display_board(board) 
+  hori = "-----------"
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts hori
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts hori
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
